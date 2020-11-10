b050096335979ea6f2aff1989b019a6e626b26bc
anirudhv14 <60284251+anirudhv14@users.noreply.github.com>
2020-11-10 12:01:01 +0530

    Update GBasis_Trial_1.m
    polyreduce function added

--- GBasis_Trial_1.m
+++ GBasis_Trial_1.m
@@ -7,3 +7,14 @@ gbasis(q);
 r = [x*y-1, y^2-1];
 G = gbasis(r);
 Gd = [G(1,2) G(1,1)]
+
+
+syms x y
+f = x*y^2-y^3+x^2-1;
+d1 = [x*y - 1, y^2 - 1];
+d2 = [y^2 - 1, x*y - 1];
+[r1,q1] = polynomialReduce(f,d1)
+[r2,q2] = polynomialReduce(f,d2)
+r = [x*y-1, y^2-1];
+G = gbasis(r);
+Gd = [G(1,2) G(1,1)]
